46cf5a38b6d9434a913f9aa63636c8fc028aa662
Denis Misuyra <script972@gmail.com>
2016-10-10 15:21:32 +0300

    second commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,14 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,6 +19,8 @@ public class Controller implements Initializable {
     private Canvas canvas;
     @FXML
     private Pane centerPane;
+    @FXML
+    private TextField LW;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -28,13 +33,19 @@ public class Controller implements Initializable {
     @FXML
     private void draw() {
         GraphicsContext gc=canvas.getGraphicsContext2D();
+        gc.clearRect(0,0,500,500);
         gc.setFill(Color.AQUA);
+        brazenchem(30,30,30,200,gc);
 
-        brazenchem(0,0,100,100,gc);
-        System.out.println("LOL");
+
 
     }
     private void brazenchem(int x1, int y1, int x2, int y2, GraphicsContext gc){
+        /*formView*/
+        int lw=0;
+        if (LW.getText()==null)
+            lw=1;
+        else lw= Integer.parseInt(LW.getText());
         int xErr=0; int yErr=0;
         int dx=x2-x1; int dy=y2-y1;
         int incX=0; int incY=0;
@@ -50,7 +61,7 @@ public class Controller implements Initializable {
         else  d=dy;
         int x=x1;
         int y=y1;
-        gc.fillRect(x,y,1,1);
+        gc.fillRect(x,y,lw,lw);
         for (int i = 0; i < d; i++) {
             xErr=xErr+dx;
             yErr=yErr+dy;
@@ -62,12 +73,16 @@ public class Controller implements Initializable {
                 yErr=yErr-d;
                 y=y+incY;
             }
-            gc.fillRect(x,y,4,4);
+            gc.fillRect(x,y,lw, lw);
         }
-
-
-
     }
 
 
+    public void incValueP(ActionEvent actionEvent) {
+
+    }
+
+    public void incValueM(ActionEvent actionEvent) {
+
+    }
 }
