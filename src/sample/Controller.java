8a08f37c3e2a23c10bb2788ced3e5e341b35cab7
Denis Misuyra <script972@gmail.com>
2016-10-16 23:40:24 +0300

    16.10.16 finished

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -15,12 +16,16 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+
     @FXML
     private Canvas canvas;
     @FXML
     private Pane centerPane;
     @FXML
-    private TextField LW;
+    private ColorPicker bgColor;
+    @FXML
+    private TextField widthF;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -33,19 +38,25 @@ public class Controller implements Initializable {
     @FXML
     private void draw() {
         GraphicsContext gc=canvas.getGraphicsContext2D();
-        gc.clearRect(0,0,500,500);
-        gc.setFill(Color.AQUA);
-        brazenchem(30,30,30,200,gc);
+        gc.clearRect(0,0,900,900);
+        gc.setFill(bgColor.getValue());
+        /*Liter M*/
+        brazenchem(30,30,30,400,gc); //left line
+        brazenchem(30,30,140,200,gc);//left 45 line
+        brazenchem(240,30,240,400,gc);//right line
+        brazenchem(240,30,140,200,gc);//right 45 line
+
+
+        brenenchemCircul(600,125,92, gc);
+        brenenchemCircul(600,318,92, gc);
+
 
 
 
     }
     private void brazenchem(int x1, int y1, int x2, int y2, GraphicsContext gc){
-        /*formView*/
-        int lw=0;
-        if (LW.getText()==null)
-            lw=1;
-        else lw= Integer.parseInt(LW.getText());
+        int width=Integer.valueOf(widthF.getText());
+
         int xErr=0; int yErr=0;
         int dx=x2-x1; int dy=y2-y1;
         int incX=0; int incY=0;
@@ -61,7 +72,7 @@ public class Controller implements Initializable {
         else  d=dy;
         int x=x1;
         int y=y1;
-        gc.fillRect(x,y,lw,lw);
+        gc.fillRect(x,y,width,width);
         for (int i = 0; i < d; i++) {
             xErr=xErr+dx;
             yErr=yErr+dy;
@@ -73,16 +84,48 @@ public class Controller implements Initializable {
                 yErr=yErr-d;
                 y=y+incY;
             }
-            gc.fillRect(x,y,lw, lw);
+            gc.fillRect(x,y,width,width);
+        }
+    }
+
+    private void brenenchemCircul(int X1, int Y1, int R, GraphicsContext gc)
+    {
+        int width=Integer.valueOf(widthF.getText());
+
+        int x= 0;
+
+        int y = R;
+        int delta= 1 - 2 * R;
+        int error = 0;
+        while (y >= 0) {
+           // if (y<R){y--; continue;}
+            gc.fillRect(X1 + x, Y1 + y, width, width);
+            gc.fillRect(X1 + x, Y1 - y, width, width);
+            //gc.fillRect(X1 - x, Y1 + y,10,10); //3
+            //gc.fillRect(X1 - x, Y1 - y,10,10);//2
+            error = 2 * (delta + y) - 1;
+            if ((delta < 0) && (error <= 0)) {
+                delta += 2 * ++x + 1;
+                continue;
+            }
+                    error=2 * (delta - x) - 1;
+            if ((delta > 0) && (error > 0)) {
+                delta += 1 - 2 * --y;
+                continue;
+            }
+                x++;
+                delta += 2 * (x - y);
+
+            y--;
         }
     }
 
 
-    public void incValueP(ActionEvent actionEvent) {
+    public void inc(ActionEvent actionEvent) {
+        widthF.setText(String.valueOf((Integer.valueOf(widthF.getText())+1)));
 
     }
-
-    public void incValueM(ActionEvent actionEvent) {
-
+    public void dec(ActionEvent actionEvent) {
+        widthF.setText(String.valueOf((Integer.valueOf(widthF.getText())-1)));
     }
 }
